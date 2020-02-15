0f2b825d0f47e4d3ce7c730ed68d62ca725433f0
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2020-02-15 04:16:12 -0500

    Update docstrings in classes & subclasses.

--- python/snewpy/FlavorTransformation.py
+++ python/snewpy/FlavorTransformation.py
@@ -1,62 +1,80 @@
 # -*- coding: utf-8 -*-
 """Simple supernova oscillation physics.
+
+For measured mixing angles and latest global analysis results, visit
+http://www.nu-fit.org/.
 """
 
 from abc import abstractmethod, ABC
 
 import numpy as np
 
+
 theta12 = np.deg2rad(33.) 
 theta13 = np.deg2rad(9.) 
 theta23 = np.deg2rad(45.)
 
+
 class FlavorTransformation(ABC):
+    """Generic interface to compute neutrino and antineutrino survival
+    probability."""
 
     @abstractmethod
     def p(self):
+        """Neutrino survival probability."""
         pass
     
     @abstractmethod
     def pbar(self):
+        """Antineutrino survival probability."""
         pass    
 
 
 class NoTransformation(FlavorTransformation):
-      def p(self):
-          return 1.
+    """Survival probabilities for no oscillation case."""
+
+    def p(self):
+        return 1.
     
-      def pbar(self):
-          return 1.
+    def pbar(self):
+        return 1.
 
 
 class AdiabaticMSW_NMO(FlavorTransformation):
-      def p(self):
-          return pow(np.sin(theta13),2.)
-    
-      def pbar(self):
-          return pow(np.cos(theta12)*np.cos(theta13),2.)    
+    """Adiabatic MSW effect, assuming normal mass ordering."""
 
+    def p(self):
+        return pow(np.sin(theta13),2.)
+    
+    def pbar(self):
+        return pow(np.cos(theta12)*np.cos(theta13),2.)    
 
 
 class AdiabaticMSW_IMO(FlavorTransformation):
-      def p(self):
-          return pow(np.sin(theta12)*np.cos(theta13),2.)
-    
-      def pbar(self):
-          return pow(np.sin(theta13),2.)
+    """Adiabatic MSW effect, assuming inverted mass ordering."""
+
+    def p(self):
+        return pow(np.sin(theta12)*np.cos(theta13),2.)
+
+    def pbar(self):
+        return pow(np.sin(theta13),2.)
 
 
 class TwoFlavorDecoherence(FlavorTransformation):
-      def p(self):
-          return 0.5
+    """Star-earth transit survival probability: two flavor case."""
+
+    def p(self):
+        return 0.5
     
-      def pbar(self):
-          return 0.5
+    def pbar(self):
+        return 0.5
 
 
 class ThreeFlavorDecoherence(FlavorTransformation):
-      def p(self):
-          return 1./3.
+    """Star-earth transit survival probability: three flavor case."""
+
+    def p(self):
+        return 1./3.
     
-      def pbar(self):
-          return 1./3.
+    def pbar(self):
+        return 1./3.
