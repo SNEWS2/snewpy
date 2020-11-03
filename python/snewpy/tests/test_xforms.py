592350ab1080e79c3511486b35246bdece7aa04b
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2020-11-03 12:15:47 -0500

    Cosmetic cleanup.

--- python/snewpy/tests/test_xforms.py
+++ python/snewpy/tests/test_xforms.py
@@ -10,23 +10,30 @@ def test_mixing_angles():
     assert(theta23 == 49.0 * u.degree)
 
 def test_noxform():
+    # No transformation.
     xform = NoTransformation()
     assert(xform.p()==1 and xform.pbar()==1)
 
 def test_adiabaticmsw_nmo():
+    # Adiabatic MSW (normal ordering).
     xform = AdiabaticMSW_NMO()
-    assert(xform.p()==sin(theta13)**2 and
-           xform.pbar()==(cos(theta12)*cos(theta13))**2)
+    assert(xform.p()==sin(theta13)**2)
+    assert(xform.pbar()==(cos(theta12)*cos(theta13))**2)
 
 def test_adiabaticmsw_imo():
+    # Adiabatic MSW (inverted ordering).
     xform = AdiabaticMSW_IMO()
-    assert(xform.p()==(sin(theta12)*cos(theta13))**2 and
-           xform.pbar()==sin(theta13)**2)
+    assert(xform.p()==(sin(theta12)*cos(theta13))**2)
+    assert(xform.pbar()==sin(theta13)**2)
 
 def test_2fd():
+    # Two-flavor decoherence.
     xform = TwoFlavorDecoherence()
-    assert(xform.p()==0.5 and xform.pbar()==0.5)
+    assert(xform.p()==0.5)
+    assert(xform.pbar()==0.5)
 
 def test_3fd():
+    # Three-flavor decoherence.
     xform = ThreeFlavorDecoherence()
-    assert(xform.p()==1./3 and xform.pbar()==1./3)
+    assert(xform.p()==1./3)
+    assert(xform.pbar()==1./3)
