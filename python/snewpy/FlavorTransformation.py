1214fd8a90713a524ef3b1ade1eee23ea2b6d8c9
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2020-11-03 12:05:41 -0500

    Use astropy units. Update subclass docstrings.

--- python/snewpy/FlavorTransformation.py
+++ python/snewpy/FlavorTransformation.py
@@ -8,11 +8,15 @@ http://www.nu-fit.org/.
 from abc import abstractmethod, ABC
 
 import numpy as np
+from astropy import units as u
 
 
-theta12 = np.deg2rad(33.) 
-theta13 = np.deg2rad(9.) 
-theta23 = np.deg2rad(45.)
+# Mixing angles, in degrees. Data source:
+# - JHEP 09 (2020) 178 [arXiv:2007.14792]
+# - NuFIT 5.0 (2020), www.nu-fit.org
+theta12 = 33.44 * u.degree
+theta13 =  8.57 * u.degree
+theta23 = 49.0  * u.degree
 
 
 class FlavorTransformation(ABC):
@@ -34,9 +38,11 @@ class NoTransformation(FlavorTransformation):
     """Survival probabilities for no oscillation case."""
 
     def p(self):
+        """Neutrino survival probability."""
         return 1.
     
     def pbar(self):
+        """Antineutrino survival probability."""
         return 1.
 
 
@@ -44,9 +50,11 @@ class AdiabaticMSW_NMO(FlavorTransformation):
     """Adiabatic MSW effect, assuming normal mass ordering."""
 
     def p(self):
+        """Neutrino survival probability."""
         return np.sin(theta13)**2
     
     def pbar(self):
+        """Antineutrino survival probability."""
         return (np.cos(theta12)*np.cos(theta13))**2    
 
 
@@ -54,9 +62,11 @@ class AdiabaticMSW_IMO(FlavorTransformation):
     """Adiabatic MSW effect, assuming inverted mass ordering."""
 
     def p(self):
+        """Neutrino survival probability."""
         return (np.sin(theta12)*np.cos(theta13))**2
 
     def pbar(self):
+        """Antineutrino survival probability."""
         return np.sin(theta13)**2
 
 
@@ -64,9 +74,11 @@ class TwoFlavorDecoherence(FlavorTransformation):
     """Star-earth transit survival probability: two flavor case."""
 
     def p(self):
+        """Neutrino survival probability."""
         return 0.5
     
     def pbar(self):
+        """Antineutrino survival probability."""
         return 0.5
 
 
@@ -74,7 +86,9 @@ class ThreeFlavorDecoherence(FlavorTransformation):
     """Star-earth transit survival probability: three flavor case."""
 
     def p(self):
+        """Neutrino survival probability."""
         return 1./3.
     
     def pbar(self):
+        """Antineutrino survival probability."""
         return 1./3.
