b057ec3cc42ba4262ada4ac07031225ec0ad3eae
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2020-11-04 21:36:40 -0500

    Load classes from neutrino module.

--- python/snewpy/flavor_transformation.py
+++ python/snewpy/flavor_transformation.py
@@ -6,67 +6,12 @@ http://www.nu-fit.org/.
 """
 
 from abc import abstractmethod, ABC
-from enum import IntEnum
 
 import numpy as np
 from astropy import units as u
 from astropy import constants as c
 
-
-class MassHierarchy(IntEnum):
-    """Neutrino mass ordering: normal or inverted.
-    """
-    NORMAL = 1
-    INVERTED = 2
-
-
-class MixingParameters:
-    """Best-fit parameters of the PMNS matrix and mass differences, assuming
-    three neutrino flavors. See www.nu-fit.org for current global fits.
-    """
-    def __init__(self, mh=MassHierarchy.NORMAL):
-        """Initialize the neutrino mixing parameters.
-
-        Parameters
-        ----------
-        mh : MassHierarchy
-            Desired mass ordering: NORMAL or INVERTED.
-        """
-        if type(mh) == MassHierarchy:
-            self.mass_order = mh
-        else:
-            raise TypeError('mh must be of type MassHierarchy')
-
-        # Values from JHEP 09 (2020) 178 [arXiv:2007.14792] and www.nu-fit.org.
-        # The reported precision is not significant given current
-        # uncertainties, but is useful for comparing to the table of
-        # parameters presented on nu-fit.org.
-        if self.mass_order == MassHierarchy.NORMAL:
-            # Note: in NH, the mass splittings are: m1..m2..............m3.
-            self.theta12 = 33.44 * u.deg
-            self.theta13 =  8.57 * u.deg
-            self.theta23 = 49.20 * u.deg
-            self.deltaCP = 197 * u.deg
-            self.dm21_2  = 7.42e-5 * u.eV**2
-            self.dm32_2  = 2.517e-3 * u.eV**2
-        else:
-            # Note: in IH, the mass splittings are: m3..............m1..m2.
-            self.theta12 = 33.45 * u.deg
-            self.theta13 =  8.60 * u.deg
-            self.theta23 = 49.30 * u.deg
-            self.deltaCP = 282 * u.deg
-            self.dm21_2  = 7.42e-5 * u.eV**2
-            self.dm31_2  = -2.498e-3 * u.eV**2
-
-    def get_mixing_angles(self):
-        """Mixing angles of the PMNS matrix.
-        
-        Returns
-        -------
-        angles : tuple
-            Angles theta12, theta13, theta23.
-        """
-        return (self.theta12, self.theta13, self.theta23)
+from .neutrino import MassHierarchy, MixingParameters
 
 
 class FlavorTransformation(ABC):
