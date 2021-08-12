f4bdff557a843020710d6c5f76f06e465509980c
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2021-08-12 10:21:43 -0400

    Use unittest assert function.

--- python/snewpy/test/test_neutrino.py
+++ python/snewpy/test/test_neutrino.py
@@ -12,43 +12,43 @@ class TestNeutrino(unittest.TestCase):
 
     def test_flavor(self):
         nue  = Flavor.NU_E
-        assert(nue.is_electron)
-        assert(nue.is_neutrino)
-        assert(not nue.is_antineutrino)
+        self.assertTrue(nue.is_electron)
+        self.assertTrue(nue.is_neutrino)
+        self.assertTrue(not nue.is_antineutrino)
 
         nux  = Flavor.NU_X
-        assert(not nux.is_electron)
-        assert(nux.is_neutrino)
-        assert(not nux.is_antineutrino)
+        self.assertTrue(not nux.is_electron)
+        self.assertTrue(nux.is_neutrino)
+        self.assertTrue(not nux.is_antineutrino)
 
         nueb = Flavor.NU_E_BAR
-        assert(nueb.is_electron)
-        assert(not nueb.is_neutrino)
-        assert(nueb.is_antineutrino)
+        self.assertTrue(nueb.is_electron)
+        self.assertTrue(not nueb.is_neutrino)
+        self.assertTrue(nueb.is_antineutrino)
 
         nuxb = Flavor.NU_X_BAR
-        assert(not nuxb.is_electron)
-        assert(not nuxb.is_neutrino)
-        assert(nuxb.is_antineutrino)
+        self.assertTrue(not nuxb.is_electron)
+        self.assertTrue(not nuxb.is_neutrino)
+        self.assertTrue(nuxb.is_antineutrino)
 
 
     def test_mixing_nmo(self):
         # By default, return mixing parameters for NMO.
         mixpars = MixingParameters()
-        assert(mixpars.theta12 == 33.44 * u.deg)
-        assert(mixpars.theta13 ==  8.57 * u.deg)
-        assert(mixpars.theta23 == 49.20 * u.deg)
-        assert(mixpars.deltaCP == 197 * u.deg)
-        assert(mixpars.dm21_2  == 7.42e-5 * u.eV**2)
-        assert(mixpars.dm32_2  == 2.517e-3 * u.eV**2)
+        self.assertTrue(mixpars.theta12 == 33.44 * u.deg)
+        self.assertTrue(mixpars.theta13 ==  8.57 * u.deg)
+        self.assertTrue(mixpars.theta23 == 49.20 * u.deg)
+        self.assertTrue(mixpars.deltaCP == 197 * u.deg)
+        self.assertTrue(mixpars.dm21_2  == 7.42e-5 * u.eV**2)
+        self.assertTrue(mixpars.dm32_2  == 2.517e-3 * u.eV**2)
 
 
     def test_mixing_imo(self):
         # By default, return mixing parameters for NMO.
         mixpars = MixingParameters(MassHierarchy.INVERTED)
-        assert(mixpars.theta12 == 33.45 * u.deg)
-        assert(mixpars.theta13 ==  8.60 * u.deg)
-        assert(mixpars.theta23 == 49.30 * u.deg)
-        assert(mixpars.deltaCP == 282 * u.deg)
-        assert(mixpars.dm21_2  == 7.42e-5 * u.eV**2)
-        assert(mixpars.dm31_2  == -2.498e-3 * u.eV**2)
+        self.assertTrue(mixpars.theta12 == 33.45 * u.deg)
+        self.assertTrue(mixpars.theta13 ==  8.60 * u.deg)
+        self.assertTrue(mixpars.theta23 == 49.30 * u.deg)
+        self.assertTrue(mixpars.deltaCP == 282 * u.deg)
+        self.assertTrue(mixpars.dm21_2  == 7.42e-5 * u.eV**2)
+        self.assertTrue(mixpars.dm31_2  == -2.498e-3 * u.eV**2)
