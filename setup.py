c60cd32cd9c738457043b8a59b26acf67d7df019
Andrey Sheshukov <dxdydz2@gmail.com>
2021-11-11 20:50:00 +0300

    Fixing error of making extras_requires

--- setup.py
+++ setup.py
@@ -57,7 +57,9 @@ with open('requirements.txt', 'r') as f:
         if line.strip():
             requires.append(line.strip())
 setup_keywords['install_requires'] = requires
-setup_keywords['docs'] = ['numpydoc']
+setup_keywords['extras_require'] ={
+    'docs':['numpydoc']
+}
 
 #
 # Internal data directories.
