364f15d40fe08c88494397b708cf22c9fe8aee2b
Andrey Sheshukov <dxdydz2@gmail.com>
2021-11-21 23:02:12 +0300

    Depracation warning when importing ccsn models directly from snewpy.models

--- python/snewpy/models/__init__.py
+++ python/snewpy/models/__init__.py
@@ -1,1 +1,9 @@
 from . import base,ccsn
+import importlib
+from warnings import warn
+
+def __getattr__(name):
+    if name in dir(ccsn):
+        warn(f"{__name__}.{name} is moved to {__name__}.ccsn.{name}")
+        return getattr(ccsn,name)
+    raise AttributeError(f"module {__name__} has no attribute {name}")
