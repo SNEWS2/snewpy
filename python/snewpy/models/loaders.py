de0a9419e423bd910a69327d3c8cd10cfc3f0373
Jost Migenda <jost.migenda@kcl.ac.uk>
2022-08-01 15:41:54 -0400

    improved separation of Nakazato registry/loader classes Remove tests of self.eos and others which are not used in the SNEWPY code itself

--- python/snewpy/models/loaders.py
+++ python/snewpy/models/loaders.py
@@ -12,10 +12,7 @@ from astropy.table import Table
 from snewpy.models.base import PinchedModel
 
 class Nakazato_2013(PinchedModel):
-    """Model based on simulations from Nakazato et al., ApJ S 205:2
-    (2013), ApJ 804:75 (2015), PASJ 73:639 (2021). See also http://asphwww.ph.noda.tus.ac.jp/snn/.
-    """
-    def __init__(self, filename):
+    def __init__(self, filename, metadata={}):
         """Model initialization.
 
         Parameters
@@ -28,26 +25,6 @@ class Nakazato_2013(PinchedModel):
         FileNotFoundError
             If a file for the chosen model parameters cannot be found
         """
-        # Store model metadata.
-        if 't_rev' in filename:
-            self.progenitor_mass = float(filename.split('-')[-1].strip('s%.fits')) * u.Msun
-            self.revival_time = float(filename.split('-')[-2].strip('t_rev%ms')) * u.ms
-            self.metallicity = float(filename.split('-')[-3].strip('z%'))
-            self.EOS = filename.split('-')[-4].upper()
-        # No revival time because the explosion "failed" (BH formation).
-        else:
-            self.progenitor_mass = float(filename.split('-')[-1].strip('s%.fits')) * u.Msun
-            self.metallicity = float(filename.split('-')[-2].strip('z%'))
-            self.revival_time = 0 * u.ms
-            self.EOS = filename.split('-')[-4].upper()
-
-        metadata = {
-            'Progenitor mass': self.progenitor_mass,
-            'EOS': self.EOS,
-            'Metallicity': self.metallicity,
-            'Revival time': self.revival_time
-        }
-
         # Read FITS table using the astropy reader.
         simtab = Table.read(filename)
         self.filename = os.path.basename(filename)
