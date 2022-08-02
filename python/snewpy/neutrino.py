88d649759b04171f2ca23e87cc286d259025badb
Jost Migenda <jost.migenda@kcl.ac.uk>
2022-08-01 23:36:09 -0400

    add 'version' argument to MixingParameters class Add NuFIT5.1 and PDG2022 mixing parameter values. For now, the default remains NuFIT5.0.

--- python/snewpy/neutrino.py
+++ python/snewpy/neutrino.py
@@ -48,39 +48,77 @@ class MixingParameters:
     [`arXiv:2007.14792 <https://arxiv.org/abs/2007.14792>`_].
     Note that the best fit values vary between normal and inverted mass hierarchy.
     """
-    def __init__(self, mh=MassHierarchy.NORMAL):
+    def __init__(self, mh=MassHierarchy.NORMAL, version="NuFIT5.0"):
         """Initialize the neutrino mixing parameters.
 
         Parameters
         ----------
         mh : MassHierarchy
             Desired mass ordering: NORMAL or INVERTED.
+        version : str
+            Version of mixing parameters. Default is "NuFIT5.0".
+            Other available values are "NuFIT5.1" and "PDG2022".
         """
         if type(mh) == MassHierarchy:
             self.mass_order = mh
         else:
             raise TypeError('mh must be of type MassHierarchy')
 
-        # Values from JHEP 09 (2020) 178 [arXiv:2007.14792] and www.nu-fit.org.
-        # The reported precision is not significant given current
-        # uncertainties, but is useful for comparing to the table of
-        # parameters presented on nu-fit.org.
-        if self.mass_order == MassHierarchy.NORMAL:
-            # Note: in NH, the mass splittings are: m1..m2..............m3.
-            self.theta12 = 33.44 * u.deg
-            self.theta13 =  8.57 * u.deg
-            self.theta23 = 49.20 * u.deg
-            self.deltaCP = 197 * u.deg
-            self.dm21_2  = 7.42e-5 * u.eV**2
-            self.dm32_2  = 2.517e-3 * u.eV**2
+        if version == "NuFIT5.1":
+            # Values from www.nu-fit.org
+            # The reported precision is not significant given current
+            # uncertainties, but is useful for comparing to the table of
+            # parameters presented on nu-fit.org.
+            if self.mass_order == MassHierarchy.NORMAL:
+                # Note: in NH, the mass splittings are: m1..m2..............m3.
+                self.theta12 = 33.45 * u.deg
+                self.theta13 = 8.62 * u.deg
+                self.theta23 = 42.10 * u.deg
+                self.deltaCP = 230 * u.deg
+                self.dm21_2 = 7.42e-5 * u.eV**2
+                self.dm32_2 = 2.510e-3 * u.eV**2
+            else:
+                # Note: in IH, the mass splittings are: m3..............m1..m2.
+                self.theta12 = 33.45 * u.deg
+                self.theta13 = 8.61 * u.deg
+                self.theta23 = 49.00 * u.deg
+                self.deltaCP = 278 * u.deg
+                self.dm21_2 = 7.42e-5 * u.eV**2
+                self.dm31_2 = -2.490e-3 * u.eV**2
+        elif version == "NuFIT5.0":
+            # Values from JHEP 09 (2020) 178 [arXiv:2007.14792] and www.nu-fit.org.
+            if self.mass_order == MassHierarchy.NORMAL:
+                self.theta12 = 33.44 * u.deg
+                self.theta13 = 8.57 * u.deg
+                self.theta23 = 49.20 * u.deg
+                self.deltaCP = 197 * u.deg
+                self.dm21_2 = 7.42e-5 * u.eV**2
+                self.dm32_2 = 2.517e-3 * u.eV**2
+            else:
+                self.theta12 = 33.45 * u.deg
+                self.theta13 = 8.60 * u.deg
+                self.theta23 = 49.30 * u.deg
+                self.deltaCP = 282 * u.deg
+                self.dm21_2 = 7.42e-5 * u.eV**2
+                self.dm31_2 = -2.498e-3 * u.eV**2
+        elif version == "PDG2022":
+            # Values from https://pdg.lbl.gov
+            if self.mass_order == MassHierarchy.NORMAL:
+                self.theta12 = 33.65 * u.deg
+                self.theta13 = 8.53 * u.deg
+                self.theta23 = 47.64 * u.deg
+                self.deltaCP = 245 * u.deg
+                self.dm21_2 = 7.53e-5 * u.eV**2
+                self.dm32_2 = 2.453e-3 * u.eV**2
+            else:
+                self.theta12 = 33.65 * u.deg
+                self.theta13 = 8.53 * u.deg
+                self.theta23 = 47.24 * u.deg
+                self.deltaCP = 245 * u.deg
+                self.dm21_2 = 7.53e-5 * u.eV**2
+                self.dm32_2 = -2.536e-3 * u.eV**2
         else:
-            # Note: in IH, the mass splittings are: m3..............m1..m2.
-            self.theta12 = 33.45 * u.deg
-            self.theta13 =  8.60 * u.deg
-            self.theta23 = 49.30 * u.deg
-            self.deltaCP = 282 * u.deg
-            self.dm21_2  = 7.42e-5 * u.eV**2
-            self.dm31_2  = -2.498e-3 * u.eV**2
+            raise ValueError(f'"{version}" is not a valid parameter version.')
 
     def get_mixing_angles(self):
         """Mixing angles of the PMNS matrix.
