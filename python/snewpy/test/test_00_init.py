b55d5a47a85a06d5e254bc8dede02ba8da3cb3ab
Spencer Griswold <sgriswold@icecube.wisc.edu>
2022-10-07 10:52:11 -0400

    Added unittests to test_init

--- python/snewpy/test/test_00_init.py
+++ python/snewpy/test/test_00_init.py
@@ -1,4 +1,7 @@
 import snewpy
-  
-def test_version_exists():
-    assert hasattr(snewpy, '__version__')
+import unittest
+
+
+class TestInit(unittest.TestCase):
+    def test_version_exists(self):
+        self.assertTrue(hasattr(snewpy, '__version__'))
