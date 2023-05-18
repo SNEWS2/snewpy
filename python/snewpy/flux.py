4482264a8b9dc1388cbf683a7138ef70e827ed8c
Andrey Sheshukov <dxdydz2@gmail.com>
2023-05-18 18:46:17 +0300

    Remove _choose_class function

--- python/snewpy/flux.py
+++ python/snewpy/flux.py
@@ -11,13 +11,18 @@ from enum import IntEnum
 from copy import copy
 
 class Axes(IntEnum):
+    """Number of the array dimension for each axis""" 
         flavor=0
         time=1
         energy=2
     
         @staticmethod
-        def __getitem__(name: str):
-            return getattr(Axes, name)
+        def __getitem__(name: Union[str,'Axes']):
+            """if given a string, return corresponding Axes value"""
+            if(isinstance(name, str)):
+                return getattr(Axes, name)
+            else: 
+                return name
             
 class Container:
     def __init__(self, data: u.Quantity,
@@ -30,27 +35,16 @@ class Container:
         self.time = time
         self.energy = energy
         
-        #if(data.shape != self._axshape):
-        #    print(f'{data.shape}!={self._axshape}')
-        
     @property
     def _axes(self):
         return self.flavor, self.time, self.energy
+
     @property
     def _axshape(self):
         return tuple(len(a) for a in self._axes)
 
-    def __getitem__(self, args):
-        """Slice the flux array and produce a new Flux object.
-        Parameters
-        ----------
-        args: slice | List[slice]
-            Slices indices for each dimension to slice on
-        Returns
-        -------
-        Flux
-            New Flux object with the array and all the dimensions are slices of initial
-        """
+    def __getitem__(self, args)->'Container':
+        """Slice the flux array and produce a new Flux object"""
         try: 
             iter(args)
         except TypeError:
@@ -62,14 +56,14 @@ class Container:
         newaxes = [ax.__getitem__(arg) for arg, ax in zip(args, self._axes)]
         return self.__class__(array, *newaxes)
     
-    def __repr__(self):
-        s = [
-            f"{len(values)} {label.name}({values.min()};{values.max()})"
-            for label, values in zip(Axes,self._axes)
-        ]
+    def __repr__(self) -> str:
+        """print information about the container"""
+        s = [f"{len(values)} {label.name}({values.min()};{values.max()})"
+            for label, values in zip(Axes,self._axes)]
         return f"{self.__class__.__name__} {self.array.shape} [{self.array.unit}]: <{' x '.join(s)}>"
     
-    def sum(self, axis: Union[Axes,str]):
+    def sum(self, axis: Union[Axes,str])->'Container':
+        """sum along given axis, producing a reduced array"""
         if(isinstance(axis, str)):
             axis = Axes[axis]
         array = np.sum(self.array, axis = axis, keepdims=True)
@@ -77,7 +71,8 @@ class Container:
         axes[axis] = axes[axis].take([0,-1])
         return ContainerClass(array.unit)(array,*axes)
     
-    def integrate(self, axis:Union[Axes,str], limits:np.ndarray = None):
+    def integrate(self, axis:Union[Axes,str], limits:np.ndarray=None)->'Container':
+        """integrate along given axis, producing a reduced array"""
         if(isinstance(axis, str)):
             axis = Axes[axis]
         #set the limits
@@ -85,36 +80,35 @@ class Container:
         xmin, xmax = ax.min(), ax.max()
         if limits is None:
             limits = u.Quantity([xmin, xmax])
-
         limits = limits.to(ax.unit)
         limits = limits.clip(xmin, xmax)
-
         #compute the integral
         x  = self._axes[axis]
         yc = cumulative_trapezoid(self.array, x=x, axis=axis, initial=0)
         _integral = interp1d(x=x, y=yc, fill_value=0, axis=axis, bounds_error=False)
-
         array = np.diff(_integral(limits),axis=axis) << (self.array.unit*ax.unit)
         axes = list(self._axes)
         axes[axis] = limits
-
         #choose the proper class
-        return _choose_class(array.unit)(array, *axes)
+        return ContainerClass(array.unit)(array, *axes)
 
     def __rmul__(self, factor):
+        "multiply array by givem factor or matrix"
         return self.__mul__(self, factor)
-        
-    def __mul__(self, factor):
+
+    def __mul__(self, factor) -> 'Container':
+        "multiply array by givem factor or matrix"
         #if not (np.isscalar(factor)):
         #    raise ValueError("Factor should be a scalar value")
         array = self.array*factor
         axes = list(self._axes)
-        return _choose_class(array.unit)(array, *axes)
+        return ContainerClass(array.unit)(array, *axes)
 
-
+#a dictionary holding classes for each unit
 _unit_classes = {}
 
 def ContainerClass(Unit, name=None):
+    """Choose appropriate container class for the given unit"""
     if Unit in _unit_classes:
         return _unit_classes[Unit]
         
@@ -135,7 +129,7 @@ def ContainerClass(Unit, name=None):
     #return the registered class
     return _unit_classes[Unit]
 
-per_unit_area = u.one/u.cm**2
+#some standard container classes that can be used for 
 Flux = ContainerClass(u.one/u.MeV/u.s/u.cm**2, "d2FdEdT")
 Fluence = ContainerClass(Flux.unit*u.s, "dFdT")
 Spectrum= ContainerClass(Flux.unit*u.MeV, "dFdE")
@@ -145,6 +139,3 @@ DifferrentialEventRate = ContainerClass(u.one/u.MeV/u.s, "d2NdEdT")
 EventRate = ContainerClass(u.one/u.s, "dNdT")
 EventSpectrum = ContainerClass(u.one/u.MeV, "dNdE")
 EventNumbers = ContainerClass(u.one, "N")
-
-def _choose_class(unit):
-    return _unit_classes.get(unit, Container)
