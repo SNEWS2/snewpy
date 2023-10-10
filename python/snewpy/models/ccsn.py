9cafd505262331d737c520332e80cf66884659e4
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2023-10-10 15:16:24 -0400

    Add test of Fornax 2022 models.

--- python/snewpy/models/ccsn.py
+++ python/snewpy/models/ccsn.py
@@ -742,6 +742,72 @@ class Fornax_2021(_RegistryModel):
     # Populate Docstring with abbreviated param values
     __new__.__doc__ = __new__.__doc__.format(**_param_abbrv)
 
+class Fornax_2022(_RegistryModel):
+    """Model based on 2D simulations of 100 progenitors from Tianshu Wang, David Vartanyan, Adam Burrows, and Matthew S.B. Coleman, MNRAS 517:543, 2022.
+       Data available at https://www.astro.princeton.edu/~burrows/nu-emissions.2d.large/
+        """
+    param = {'progenitor': 
+                [  '9.0',     '9.25',     '9.5',      '9.75',     '10.0',
+                  '10.25',    '10.5',     '10.75',    '11.0',     '11.25',
+                  '11.5',     '11.75',    '12.00.bh', '12.03.bh', '12.07.bh',
+                  '12.1.bh',  '12.13',    '12.15',    '12.18.bh', '12.20.bh',
+                  '12.25',    '12.33.bh', '12.40.bh', '12.45.bh', '12.50.bh',
+                  '12.54.bh', '12.60.bh', '12.63',    '12.70',    '12.72.bh',
+                  '12.75',    '12.80.bh', '12.85.bh', '12.90.bh', '12.93',
+                  '12.97.bh', '13.00.bh', '13.05.bh', '13.11',    '13.25.bh',
+                  '13.27.bh', '13.32.bh', '13.40.bh', '13.45',    '13.50.bh',
+                  '13.60.bh', '13.75',    '13.82.bh', '13.90.bh', '13.96',
+                  '14.01',    '14.13.bh', '14.25.bh', '14.40.bh', '14.41.bh',
+                  '14.43',    '14.44.bh', '14.70.bh', '14.87.bh', '15.00.bh',
+                  '15.01',    '15.04.bh', '15.05',    '15.38.bh', '16.43',
+                  '16.65',    '16.99',    '17.00',    '17.07',    '17.10',
+                  '17.40',    '17.48',    '17.50',    '17.51',    '17.83',
+                  '18.04',    '18.05',    '18.09',    '18.10',    '18.50',
+                  '19.02',    '19.56',    '19.83',    '19.99',    '20.08',
+                  '20.09',    '20.18',    '20.37',    '21.00',    '21.68',
+                  '22.00',    '22.30',    '22.82',    '23.00',    '23.04',
+                  '23.43',    '24.00',    '25.00',    '26.00',    '26.99']
+            }
+    param['progenitor_mass'] = [float(x[:-3]) if x.endswith('bh') else float(x) for x in param['progenitor']] * u.Msun
+
+    _param_abbrv = {
+        'progenitor': '["9.0".."26.99"]',
+        'progenitor_mass': '[9.0..26.99] solMass'}
+
+    @_warn_deprecated_filename_argument
+    def __new__(cls, filename=None, *, progenitor=None):
+        """Model Initialization.
+
+        Parameters
+        ----------
+        filename : str
+            Absolute or relative path to file with model data. This argument is deprecated.
+
+        Other Parameters
+        ----------------
+        progenitor: str
+            Progenitor type mass and result, e.g., '10.0' or 14.70.bh' Valid values are {progenitor}.
+        """
+        if filename is not None:
+            progenitor = os.path.splitext(os.path.basename(filename))[0].split('_')[2]
+            progenitor_mass = float(progenitor[:-3]) if progenitor.endswith('bh') else float(progenitor)
+            metadata = {'Progenitor' : progenitor,
+                        'Progenitor mass': progenitor_mass * u.Msun}
+            return loaders.Fornax_2022(os.path.abspath(filename), metadata)
+
+        # Load from Parameters
+        progenitor_mass = float(progenitor[:-3]) if progenitor.endswith('bh') else float(progenitor)
+        cls.check_valid_params(cls, progenitor=progenitor, progenitor_mass=progenitor_mass * u.Msun)
+        filename = f'lum_spec_{progenitor}_dat.h5'
+
+        metadata = {'Progenitor': progenitor}
+        metadata = {'Progenitor mass': progenitor_mass * u.Msun}
+
+        return loaders.Fornax_2022(filename, metadata)
+
+    # Populate Docstring with abbreviated param values
+    __new__.__doc__ = __new__.__doc__.format(**_param_abbrv)
+
 class SNOwGLoBES:
     """A model that does not inherit from SupernovaModel (yet) and imports a group of SNOwGLoBES files."""
 
