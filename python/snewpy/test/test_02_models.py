208712bfb5b1571630eca54a5850b3e1bc4991c9
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2023-10-12 20:40:56 -0400

    Remove model filename initialization test.

--- python/snewpy/test/test_02_models.py
+++ python/snewpy/test/test_02_models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Unit tests for the neutrino submodule.
+"""Unit tests for the models initializations.
 """
 import unittest
 
@@ -7,8 +7,7 @@ from snewpy.neutrino import Flavor
 from snewpy.models.loaders import Nakazato_2013, Tamborra_2014, OConnor_2013, OConnor_2015, \
                                   Sukhbold_2015, Bollig_2016, Walk_2018, \
                                   Walk_2019, Fornax_2019, Warren_2020, \
-                                  Kuroda_2020, Fornax_2021, Zha_2021, \
-                                  Fornax_2022
+                                  Kuroda_2020, Fornax_2021, Zha_2021
 from snewpy._model_urls import model_urls
 from astropy import units as u
 from snewpy import model_path
@@ -311,48 +310,4 @@ class TestModels(unittest.TestCase):
             self.assertEqual(type(f), dict)
             self.assertEqual(len(f), len(Flavor))
             self.assertEqual(f[Flavor.NU_E].unit, 1./(u.erg * u.s))
-
-    def test_Fornax_2022(self):
-        """
-        Instantiate a set of 'Fornax 2022' models
-        """
-        progenitors = [
-            '9.0',     '9.25',     '9.5',      '9.75',     '10.0',
-            '10.25',    '10.5',     '10.75',    '11.0',     '11.25',
-            '11.5',     '11.75',    '12.00.bh', '12.03.bh', '12.07.bh',
-            '12.1.bh',  '12.13',    '12.15',    '12.18.bh', '12.20.bh',
-            '12.25',    '12.33.bh', '12.40.bh', '12.45.bh', '12.50.bh',
-            '12.54.bh', '12.60.bh', '12.63',    '12.70',    '12.72.bh',
-            '12.75',    '12.80.bh', '12.85.bh', '12.90.bh', '12.93',
-            '12.97.bh', '13.00.bh', '13.05.bh', '13.11',    '13.25.bh',
-            '13.27.bh', '13.32.bh', '13.40.bh', '13.45',    '13.50.bh',
-            '13.60.bh', '13.75',    '13.82.bh', '13.90.bh', '13.96',
-            '14.01',    '14.13.bh', '14.25.bh', '14.40.bh', '14.41.bh',
-            '14.43',    '14.44.bh', '14.70.bh', '14.87.bh', '15.00.bh',
-            '15.01',    '15.04.bh', '15.05',    '15.38.bh', '16.43',
-            '16.65',    '16.99',    '17.00',    '17.07',    '17.10',
-            '17.40',    '17.48',    '17.50',    '17.51',    '17.83',
-            '18.04',    '18.05',    '18.09',    '18.10',    '18.50',
-            '19.02',    '19.56',    '19.83',    '19.99',    '20.08',
-            '20.09',    '20.18',    '20.37',    '21.00',    '21.68',
-            '22.00',    '22.30',    '22.82',    '23.00',    '23.04',
-            '23.43',    '24.00',    '25.00',    '26.00',    '26.99' ]
-
-        for progenitor in progenitors:
-            mfile = f'Fornax_2022/lum_spec_{progenitor}_dat.h5'
-            model = Fornax_2022(os.path.join(model_path, mfile))
-
-            mass = float(progenitor[:-3] if 'bh' in progenitor else progenitor) * u.Msun
-#            self.assertEqual(model.metadata['Progenitor'], progenitor)
-#            self.assertEqual(model.metadata['Progenitor mass'], mass)
-
-            # Check that times are in proper units.
-            t = model.get_time()
-            self.assertTrue(t.unit, u.s)
-
-            # Check that we can compute flux dictionaries.
-            f = model.get_initial_spectra(0*u.s, 10*u.MeV)
-            self.assertEqual(type(f), dict)
-            self.assertEqual(len(f), len(Flavor))
-            self.assertEqual(f[Flavor.NU_E].unit, 1/(u.erg * u.s))
 
