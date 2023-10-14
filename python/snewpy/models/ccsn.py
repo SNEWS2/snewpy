fc8be3dabead948b7edc017e763a4c8ea9fa6c74
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2023-10-14 14:32:02 -0400

    Remove progenitor mass from parameters.

--- python/snewpy/models/ccsn.py
+++ python/snewpy/models/ccsn.py
@@ -746,18 +746,19 @@ class Fornax_2021(_RegistryModel):
 class Mori_2023(_RegistryModel):
     """Model based on 2D simulations with axionlike particles, K. Mori, T.  Takiwaki, K. Kotake and S. Horiuchi, Phys. Rev. D 108:063027, 2023. All models are based on the non-rotating 20 M_sun solar metallicity progenitor model from S.E. Woolsey and A. Heger, Phys. Rep. 442:269, 2007. Data from private communication.
         """
-    param = {'axion_mass' : [0, 100, 200]}
-    param = {'axion_coupling' : [0, 2, 4, 6, 8, 10, 12, 14, 16, 20]}
-    param = {'progenitor_mass': 20 * u.Msun }
+    param = {'axion_mass' : [0, 100, 200],
+             'axion_coupling' : [0, 2, 4, 6, 8, 10, 12, 14, 16, 20] }
 
-    #_param_validator = lambda p: True
+    _param_validator = lambda p: \
+        (p['axion_mass'] == 0 and p['axion_coupling'] == 0) or \
+        (p['axion_mass'] == 100 and p['axion_coupling'] in (2,4,10,12,14,16,20)) or \
+        (p['axion_mass'] == 200 and p['axion_coupling'] in (2,4,6,8,10,20))
 
     _param_abbrv = {
         'axion_mass': '[0, 100, 200] MeV',
-        'axion_coupling' : '[0..20] 1e-10/GeV', 
-        'progenitor_mass': '20 solMass'}
+        'axion_coupling' : '[0..20] 1e-10/GeV' }
 
-    def __new__(cls, axion_mass, axion_coupling, *, progenitor_mass=None):
+    def __new__(cls, axion_mass, axion_coupling):
         """Model Initialization.
 
         Parameters
@@ -768,18 +769,17 @@ class Mori_2023(_RegistryModel):
             Axion-photon coupling, in units of 1e-10/GeV. Valid values are {axion_coupling}.
         """
         # Load from Parameters
-        #cls.check_valid_params(cls, axion_mass=axion_mass, axion_coupling=axion_coupling)
+        cls.check_valid_params(cls, axion_mass=axion_mass, axion_coupling=axion_coupling)
 
-        axmass = axion_mass#f'{axion_mass.to_value("MeV"):d}'
         if axion_mass == 0:
             filename = 't-prof_std.dat'
         else:
             filename = f't-prof_{axion_mass}_{axion_coupling}.dat'
 
         metadata = {
-            'Axion mass': axion_mass,
-            'Axion coupling': axion_coupling,
-            'Progenitor mass': progenitor_mass if progenitor_mass else cls.param['progenitor_mass'] }
+            'Axion mass': axion_mass * u.MeV,
+            'Axion coupling': axion_coupling * 1e-10/u.GeV,
+            'Progenitor mass': 20*u.Msun}
 
         return loaders.Mori_2023(filename, metadata)
 
