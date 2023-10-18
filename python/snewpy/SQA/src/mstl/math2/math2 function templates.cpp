75a01d91e3474794b5c810459aa87bc4ea20cb95
jpkneller <jpknelle@ncsu.edu>
2023-10-18 10:32:12 -0400

    Add files via upload

--- python/snewpy/SQA/src/mstl/math2/math2 function templates.cpp
+++ python/snewpy/SQA/src/mstl/math2/math2 function templates.cpp
@@ -7,35 +7,19 @@ template <class Type> Type Theta(const Type &T){ if(T<=Zero<Type>()){ return Zer
 //*****************************************************************************************
 //*****************************************************************************************
 
-template <class Type> bool Equality(Type A,Type B,int N)
-         { if(std::isinf(A)==true || std::isinf(B)==true){ return A == B;}
-           if(std::isnan(A)==true || std::isnan(B)==true){ return A == B;}
-           if(std::numeric_limits<Type>::is_integer==true){ return A == B;}
+template<class Type> typename std::enable_if<!std::numeric_limits<Type>::is_integer, bool>::type Equality(Type A,Type B,unsigned int N)
+        { int i=0; 
+          if(A>B){ while(i<N){ B=nextafter(B,+INFINITY); i++;}; return B >= A;}
+          else{ while(i<N){ A=nextafter(A,+INFINITY); i++;}; return A >= B;}
+         }
 
-           //convert to bit patterns 
-           int i; 
-           //std::bitset<sizeof(A)*CHAR_BIT> a(*reinterpret_cast<unsigned long*>(&A));
-           //i=1; while(i*sizeof(unsigned long)<sizeof(A)){ a|=(std::bitset<sizeof(A)*CHAR_BIT>(*(reinterpret_cast<unsigned long*>(&A)+i))<<sizeof(unsigned long)*CHAR_BIT); i++;};
-           //std::bitset<sizeof(B)*CHAR_BIT> b(*reinterpret_cast<unsigned long*>(&B));
-           //i=1; while(i*sizeof(unsigned long)<sizeof(B)){ b|=(std::bitset<sizeof(B)*CHAR_BIT>(*(reinterpret_cast<unsigned long*>(&B)+i))<<sizeof(unsigned long)*CHAR_BIT); i++;};
-           std::bitset<sizeof(A)*CHAR_BIT> a(*reinterpret_cast<char*>(&A));
-           i=1; while(i*sizeof(unsigned long)<sizeof(A)){ a|=(std::bitset<sizeof(A)*CHAR_BIT>(*(reinterpret_cast<char*>(&A)+i))<<sizeof(char)*CHAR_BIT); i++;};
-           std::bitset<sizeof(B)*CHAR_BIT> b(*reinterpret_cast<char*>(&B));
-           i=1; while(i*sizeof(unsigned long)<sizeof(B)){ b|=(std::bitset<sizeof(B)*CHAR_BIT>(*(reinterpret_cast<char*>(&B)+i))<<sizeof(char)*CHAR_BIT); i++;};
+template<class Type> typename std::enable_if<!std::numeric_limits<Type>::is_integer, bool>::type Equality(Type A,std::complex<Type> B,unsigned int N)
+         { return Equality(A,real(B),N) & Equality(Zero(A),imag(B),N);}
 
+template<class Type> typename std::enable_if<!std::numeric_limits<Type>::is_integer, bool>::type Equality(std::complex<Type> A,Type B,unsigned int N)
+         { return Equality(real(A),B,N) & Equality(imag(A),Zero(B),N);}
 
-           if(std::numeric_limits<Type>::is_signed==true) //flip from twos complement if negative
-             { if(a[a.size()-1]==true){ a.flip()++;} 
-               if(b[b.size()-1]==true){ b.flip()++;}
-              }
-
-           std::bitset<sizeof(B)*CHAR_BIT> d(a^b); // the difference between the two bit patterns
-           for(i=0;i<=N-1;i++){ d.reset(i);}       // reset first N elements to zero
-           if(d.any()==false){ return true;}       // if all other bits are zero then the two bit patterns are equal
-           return false;
-          }
-
-template <class Type> bool Equality(std::complex<Type> A,std::complex<Type> B,int N)
+template<class Type> typename std::enable_if<!std::numeric_limits<Type>::is_integer, bool>::type Equality(std::complex<Type> A,std::complex<Type> B,unsigned int N)
          { return Equality(real(A),real(B),N) & Equality(imag(A),imag(B),N);}
 
 #endif
--- python/snewpy/SQA/src/mstl/math2/math2.h
+++ python/snewpy/SQA/src/mstl/math2/math2.h
@@ -6,6 +6,9 @@
 #include <ctime>
 #include <cmath>
 #include <complex>
+#include <type_traits>
+#include <algorithm>
+#include <limits>       
 //#include <values.h>
 
 #include "mstl.h"
@@ -18,8 +21,21 @@
 //*****************************************************************************************************
 
 // additional math functions
-template <class Type> bool Equality(Type A,Type B,int N=4);
-template <class Type> bool Equality(std::complex<Type> A,std::complex<Type> B,int N=4);
+template <class Type> Type Theta(const Type &T); // Heaviside Step Function;
+
+int Kronecker(int i,int j); // return 1 if i==j, 0 otherwise
+
+double Heaviside(double x,double x0); 
+double tophat(double x,double xmax); 
+
+
+template<class Type> typename std::enable_if<!std::numeric_limits<Type>::is_integer, bool>::type Equality(Type A,Type B,unsigned int N=4);
+
+template<class Type> typename std::enable_if<!std::numeric_limits<Type>::is_integer, bool>::type Equality(Type A,std::complex<Type> B,unsigned int N=4);
+
+template<class Type> typename std::enable_if<!std::numeric_limits<Type>::is_integer, bool>::type Equality(std::complex<Type> A,Type B,unsigned int N=4);
+
+template<class Type> typename std::enable_if<!std::numeric_limits<Type>::is_integer, bool>::type Equality(std::complex<Type> A,std::complex<Type> B,unsigned int N=4);
 
 //*****************************************************************************************************
 //*****************************************************************************************************
