a414db6ceeff531072115a0af06a4b81dda1f0e7
Andrey Sheshukov <dxdydz2@gmail.com>
2023-12-13 17:03:43 +0300

    Update links in docstrings

--- python/snewpy/models/presn.py
+++ python/snewpy/models/presn.py
@@ -13,6 +13,11 @@ from astropy import units as u
     progenitor_mass = [15, 25]<<u.Msun
 )
 class Odrzywolek_2010(loaders.Odrzywolek_2010):
+    """Presupernova model based on 
+    `A.Odrzywolek, Alexander Heger, Acta Phys.Polon.B 41 (2010) <https://inspirehep.net/literature/870759>`_
+    
+    Dataset available at `website <http://th.if.uj.edu.pl/~odrzywolek/psns/index.html>`_ 
+    """
     def __init__(self, progenitor_mass:u.Quantity):
         filename=f"s{progenitor_mass.to_value('Msun'):.0f}_nuebar_data.txt"
         super().__init__(filename)
@@ -21,6 +26,11 @@ class Odrzywolek_2010(loaders.Odrzywolek_2010):
     progenitor_mass = [15, 30]<<u.Msun
 )
 class Patton_2017(loaders.Patton_2017):
+    """Presupernova model based on 
+    `Kelly M. Patton et al 2017 ApJ 851 6 <https://iopscience.iop.org/article/10.3847/1538-4357/aa95c4>`_
+    
+    Dataset available at `Zenodo <http://doi.org/10.5281/zenodo.2626645>`_
+    """
     def __init__(self, progenitor_mass:u.Quantity):
         filename=f"totalLuminosity_{progenitor_mass.to_value('Msun'):.0f}SolarMass.dat"
         super().__init__(filename)
@@ -29,6 +39,11 @@ class Patton_2017(loaders.Patton_2017):
     progenitor_mass = [12, 15]<<u.Msun
 )
 class Kato_2017(loaders.Kato_2017):
+    """Presupernova model based on 
+    `Chinami Kato et al 2017 ApJ 848 48 <https://iopscience.iop.org/article/10.3847/1538-4357/aa8b72>`_
+    
+    Dataset available at `Zenodo <https://zenodo.org/records/3768052>`_
+    """
     def __init__(self, progenitor_mass:u.Quantity):
         path=f"pre_collapse/m{progenitor_mass.to_value('Msun'):.0f}"
         super().__init__(path)
