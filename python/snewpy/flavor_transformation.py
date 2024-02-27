22bd80c1c167e1ebce350c644be59b60c778929d
jpkneller <jpknelle@ncsu.edu>
2024-02-27 09:47:36 -0500

    Update flavor_transformation.py

--- python/snewpy/flavor_transformation.py
+++ python/snewpy/flavor_transformation.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Supernova oscillation physics for flavors e, X, e-bar, X-bar.
+"""Supernova oscillation physics 
 
 For measured mixing angles and latest global analysis results, visit
 http://www.nu-fit.org/.
@@ -56,7 +56,7 @@ class ThreeFlavorTransformation(FlavorTransformation):
 
 
     def Pmf_HighDensityLimit(self):
-        """ The probability that a given flavor state is 'detected' as a particular matter state in the 
+        """ The probability that a given flavor state is a particular matter state in the 
         infinite density limit.  
 
         Returns
@@ -87,8 +87,7 @@ class ThreeFlavorTransformation(FlavorTransformation):
               -np.abs( HV[NU_MU_BAR,NU_MU_BAR]-HV[NU_TAU_BAR,NU_TAU_BAR] )**2 / 4 
 
         """ The NMO case. Matter state 3 is the electron neutrino, matter state 1bar is the electron 
-        antineutrino. How mu and tau flavors are assigned to matter states 1 and 2 (2bar and 3bar 
-        for the antineutrinos) is given by 
+        antineutrino. 
         """
         if self.mix_params.mass_order == MassHierarchy.NORMAL:
             k1 = T - np.sqrt(D)
@@ -96,21 +95,20 @@ class ThreeFlavorTransformation(FlavorTransformation):
             k2bar = Tbar - np.sqrt(Dbar)
             k3bar = Tbar + np.sqrt(Dbar)
 
-            PmfHDL[2,NU_E] = 1 
             PmfHDL[0,NU_MU] = ( np.real(HV[NU_TAU,NU_TAU]) - k1 ) / ( k2 - k1 )
-            PmfHDL[1,NU_MU] = ( np.real(HV[NU_TAU,NU_TAU]) - k2 ) / ( k1 - k2 )
             PmfHDL[0,NU_TAU] = ( np.real(HV[NU_MU,NU_MU]) - k1 ) / ( k2 - k1 )
+            PmfHDL[1,NU_MU] = ( np.real(HV[NU_TAU,NU_TAU]) - k2 ) / ( k1 - k2 )
             PmfHDL[1,NU_TAU] = ( np.real(HV[NU_MU,NU_MU]) - k2 ) / ( k1 - k2 )
+            PmfHDL[2,NU_E] = 1             
 
             PmfHDL[3,NU_E_BAR] = 1
             PmfHDL[4,NU_MU_BAR] = ( np.real(HV[NU_TAU_BAR,NU_TAU_BAR]) - k2bar ) / ( k3bar - k2bar )
-            PmfHDL[5,NU_MU_BAR] = ( np.real(HV[NU_TAU_BAR,NU_TAU_BAR]) - k3bar ) / ( k2bar - k3bar )
             PmfHDL[4,NU_TAU_BAR] = ( np.real(HV[NU_MU_BAR,NU_MU_BAR]) - k2bar ) / ( k3bar - k2bar )
+            PmfHDL[5,NU_MU_BAR] = ( np.real(HV[NU_TAU_BAR,NU_TAU_BAR]) - k3bar ) / ( k2bar - k3bar )
             PmfHDL[5,NU_TAU_BAR] = ( np.real(HV[NU_MU_BAR,NU_MU_BAR]) - k3bar ) / ( k2bar - k3bar )
 
         """ The IMO case. Matter state 2 is the electron neutrino, matter state 3bar is the electron 
-        antineutrino. How mu and tau flavors are assigned to matter states 1 and 3 (1bar and 2bar 
-        for the antineutrinos) is given by 
+        antineutrino. 
         """
         if self.mix_params.mass_order == MassHierarchy.INVERTED:
             k1 = T + np.sqrt(D)
@@ -118,17 +116,17 @@ class ThreeFlavorTransformation(FlavorTransformation):
             k1bar = Tbar - np.sqrt(Dbar)
             k2bar = Tbar + np.sqrt(Dbar)
 
-            PmfHDL[1,NU_E] = 1 
             PmfHDL[0,NU_MU] = ( np.real(HV[NU_TAU,NU_TAU]) - k1 ) / ( k3 - k1 )
-            PmfHDL[2,NU_MU] = ( np.real(HV[NU_TAU,NU_TAU]) - k3 ) / ( k1 - k3 )
             PmfHDL[0,NU_TAU] = ( np.real(HV[NU_MU,NU_MU]) - k1 ) / ( k3 - k1 )
+            PmfHDL[1,NU_E] = 1             
+            PmfHDL[2,NU_MU] = ( np.real(HV[NU_TAU,NU_TAU]) - k3 ) / ( k1 - k3 )
             PmfHDL[2,NU_TAU] = ( np.real(HV[NU_MU,NU_MU]) - k3 ) / ( k1 - k3 )
 
-            PmfHDL[5,NU_E_BAR] = 1
             PmfHDL[3,NU_MU_BAR] = ( np.real(HV[NU_TAU_BAR,NU_TAU_BAR]) - k1bar ) / ( k2bar - k1bar )
+            PmfHDL[3,NU_TAU_BAR] = ( np.real(HV[NU_MU_BAR,NU_MU_BAR]) - k1bar ) / ( k2bar - k1bar )            
             PmfHDL[4,NU_MU_BAR] = ( np.real(HV[NU_TAU_BAR,NU_TAU_BAR]) - k2bar ) / ( k1bar - k2bar )
-            PmfHDL[3,NU_TAU_BAR] = ( np.real(HV[NU_MU_BAR,NU_MU_BAR]) - k1bar ) / ( k2bar - k1bar )
             PmfHDL[4,NU_TAU_BAR] = ( np.real(HV[NU_MU_BAR,NU_MU_BAR]) - k2bar ) / ( k1bar - k2bar )
+            PmfHDL[5,NU_E_BAR] = 1            
 
         return PmfHDL
 
@@ -189,8 +187,7 @@ class FourFlavorTransformation:
 
         """ The NMO case. Matter state 4 is the electron neutrino, matter state 1bar is the electron 
         antineutrino. Matter state 3 is the sterile neutrino, matter state 2bar is the sterile 
-        antineutrino. How mu and tau flavors are assigned to matter states 1 and 2 (3bar and 4bar 
-        for the antineutrinos) is given by the transpose of the vacuum mixing matrix
+        antineutrino. 
         """
         if self.mix_params.mass_order == MassHierarchy.NORMAL:
             k1 = T - np.sqrt(D)
@@ -198,24 +195,24 @@ class FourFlavorTransformation:
             k3bar = Tbar - np.sqrt(Dbar)
             k4bar = Tbar + np.sqrt(Dbar)
 
-            PmfHDL[3,NU_E] = 1 
             PmfHDL[0,NU_MU] = ( np.real(HV[NU_TAU,NU_TAU]) - k1 ) / ( k2 - k1 )
+            PmfHDL[0,NU_TAU] = ( np.real(HV[NU_MU,NU_MU]) - k1 ) / ( k2 - k1 )            
             PmfHDL[1,NU_MU] = ( np.real(HV[NU_TAU,NU_TAU]) - k2 ) / ( k1 - k2 )
-            PmfHDL[0,NU_TAU] = ( np.real(HV[NU_MU,NU_MU]) - k1 ) / ( k2 - k1 )
             PmfHDL[1,NU_TAU] = ( np.real(HV[NU_MU,NU_MU]) - k2 ) / ( k1 - k2 )
             PmfHDL[2,NU_S] = 1
+            PmfHDL[3,NU_E] = 1             
 
             PmfHDL[4,NU_E_BAR] = 1
+            PmfHDL[5,NU_S_BAR] = 1            
             PmfHDL[6,NU_MU_BAR] = ( np.real(HV[NU_TAU_BAR,NU_TAU_BAR]) - k3bar ) / ( k4bar - k3bar )
-            PmfHDL[7,NU_MU_BAR] = ( np.real(HV[NU_TAU_BAR,NU_TAU_BAR]) - k4bar ) / ( k3bar - k4bar )
             PmfHDL[6,NU_TAU_BAR] = ( np.real(HV[NU_MU_BAR,NU_MU_BAR]) - k3bar ) / ( k4bar - k3bar )
+            PmfHDL[7,NU_MU_BAR] = ( np.real(HV[NU_TAU_BAR,NU_TAU_BAR]) - k4bar ) / ( k3bar - k4bar )
             PmfHDL[7,NU_TAU_BAR] = ( np.real(HV[NU_MU_BAR,NU_MU_BAR]) - k4bar ) / ( k3bar - k4bar )
-            PmfHDL[5,NU_S_BAR] = 1
+
 
         """ The IMO case. Matter state 4 is the electron neutrino, matter state 3bar is the electron 
         antineutrino. Matter state 2 is the sterile neutrino, matter state 1bar is the sterile 
-        antineutrino. How mu and tau flavors are assigned to matter states 1 and 3 (2bar and 4bar 
-        for the antineutrinos) is given by the transpose of the vacuum mixing matrix
+        antineutrino. 
         """
         if self.mix_params.mass_order == MassHierarchy.INVERTED:
             k1 = T + np.sqrt(D)
@@ -223,20 +220,19 @@ class FourFlavorTransformation:
             k2bar = Tbar - np.sqrt(Dbar)
             k4bar = Tbar + np.sqrt(Dbar)
 
-            PmfHDL[3,NU_E] = 1 
             PmfHDL[0,NU_MU] = ( np.real(HV[NU_TAU,NU_TAU]) - k1 ) / ( k3 - k1 )
+            PmfHDL[0,NU_TAU] = ( np.real(HV[NU_MU,NU_MU]) - k1 ) / ( k3 - k1 )            
+            PmfHDL[1,NU_S] = 1             
             PmfHDL[2,NU_MU] = ( np.real(HV[NU_TAU,NU_TAU]) - k3 ) / ( k1 - k3 )
-            PmfHDL[0,NU_TAU] = ( np.real(HV[NU_MU,NU_MU]) - k1 ) / ( k3 - k1 )
             PmfHDL[2,NU_TAU] = ( np.real(HV[NU_MU,NU_MU]) - k3 ) / ( k1 - k3 )
-            PmfHDL[1,NU_S] = 1 
+            PmfHDL[3,NU_E] = 1 
 
-            PmfHDL[6,NU_E_BAR] = 1
+            PmfHDL[4,NU_S_BAR] = 1            
             PmfHDL[5,NU_MU_BAR] = ( np.real(HV[NU_TAU_BAR,NU_TAU_BAR]) - k2bar ) / ( k4bar - k2bar )
-            PmfHDL[7,NU_MU_BAR] = ( np.real(HV[NU_TAU_BAR,NU_TAU_BAR]) - k4bar ) / ( k2bar - k4bar )
-            PmfHDL[5,NU_TAU_BAR] = ( np.real(HV[NU_MU_BAR,NU_MU_BAR]) - k2bar ) / ( k4bar - k2bar )
+            PmfHDL[5,NU_TAU_BAR] = ( np.real(HV[NU_MU_BAR,NU_MU_BAR]) - k2bar ) / ( k4bar - k2bar )            
+            PmfHDL[6,NU_E_BAR] = 1
             PmfHDL[7,NU_TAU_BAR] = ( np.real(HV[NU_MU_BAR,NU_MU_BAR]) - k4bar ) / ( k2bar - k4bar )
-            PmfHDL[4,NU_S_BAR] = 1
-
+            PmfHDL[7,NU_MU_BAR] = ( np.real(HV[NU_TAU_BAR,NU_TAU_BAR]) - k4bar ) / ( k2bar - k4bar )    
 
         return PmfHDL
 
@@ -378,7 +374,7 @@ class AdiabaticMSW(ThreeFlavorTransformation):
 ###############################################################################
 
 class NonAdiabaticMSWH(ThreeFlavorTransformation):
-    """Nonadiabatic MSW effect."""
+    """Nonadiabatic MSW H resonance"""
 
     def __init__(self, mix_params = None):
         """Initialize flavor transformation
@@ -445,7 +441,7 @@ class NonAdiabaticMSWH(ThreeFlavorTransformation):
 ###############################################################################
 
 class TwoFlavorDecoherence(ThreeFlavorTransformation):
-    """Star-earth transit survival probability: two flavor case."""
+    """equal mixing of whatever two matter states form the MSW H resonance"""
 
     def __init__(self, mix_params = None):
         """Initialize flavor transformation
@@ -512,7 +508,7 @@ class TwoFlavorDecoherence(ThreeFlavorTransformation):
 ###############################################################################
 
 class ThreeFlavorDecoherence(ThreeFlavorTransformation):
-    """Star-earth transit survival probability: three flavor case."""
+    """Equal mixing of all threen eutrino matter states and antineutrino matter states"""
 
     def __init__(self):
         """Initialize ThreeFlavorTransformation to default case"""
@@ -540,6 +536,7 @@ class ThreeFlavorDecoherence(ThreeFlavorTransformation):
         return p
 
     def get_probabilities(self, t, E): 
+        """Equal mixing so Earth matter has no effect"""
         return self.get_SNprobabilities(t,E) 
 
 ###############################################################################
@@ -550,7 +547,9 @@ except:
     SNOSHEWS = None
 
 class SNprofile:
-    """Simple container for density and electron fraction profiles"""
+    """A placeholder class for the density and electron fraction profiles. Currently SNOSHEWS 
+    reads the profiles from files but that might change in the future.
+    """
 
     def __init__(self, rhofilename, Yefilename):         
         self.rhofilename = rhofilename
@@ -568,18 +567,18 @@ class MSWEffect(ThreeFlavorTransformation):
         ----------
         SNprofile : instance of profile class
         mix_params : ThreeFlavorMixingParameters instance or None
+        rmin : starting radius for calculation. rmin will be corrected by SNOSHEWS to the minimum radius of the profile if rmin is less than that value        
+        rmax : the ending radius of the calculation. rmax will be corrected by SNOSHEWS to the maximum radius of the profile if rmax is greater than that value
         """
         self.SNprofile = SNprofile
      
         super().__init__(mix_params) 
 
-        # rmmin will be corrected by SNOSHEWS to the minimum radius of the profile if rmin is less than that value        
         if rmin == None:
             rmin = 0 
         else: 
             self.rmin = rmin
 
-        # rmax will be corrected by SNOSHEWS to the maximum radius of the profile if rmax is greater than that value
         if rmax == None:
             rmax = 1e99 
         else: 
@@ -644,8 +643,8 @@ class MSWEffect(ThreeFlavorTransformation):
 
         # restructure the results
         Pmf = np.zeros((6,6,ID.NE))
-        m = 0
-        while m<ID.NE:
+
+        for m in range(ID.NE):
             Pmf[0,ThreeFlavor.NU_E,m] = pSN[0][m][0][0] 
             Pmf[1,ThreeFlavor.NU_E,m] = pSN[0][m][1][0]
             Pmf[2,ThreeFlavor.NU_E,m] = pSN[0][m][2][0]
@@ -665,7 +664,6 @@ class MSWEffect(ThreeFlavorTransformation):
             Pmf[3,ThreeFlavor.NU_TAU_BAR,m] = pSN[1][m][0][2] 
             Pmf[4,ThreeFlavor.NU_TAU_BAR,m] = pSN[1][m][1][2]
             Pmf[5,ThreeFlavor.NU_TAU_BAR,m] = pSN[1][m][2][2]
-            m += 1
             
     return Pmf
 
@@ -853,7 +851,7 @@ class NonAdiabaticMSWes(FourFlavorTransformation):
 ###############################################################################
 
 """ The modifier transformtions. These cannot be used by themselves but can be 
-    combined with a flavor transformtion class from the first group using the Catenate class
+    combined with a flavor transformtion from those defined above using the Catenate class
 """
 
 class NeutrinoDecay(ThreeFlavorTransformation):
@@ -1041,8 +1039,7 @@ class ThreeFlavorNoEarthMatter(ThreeFlavorTransformation):
 
         Returns
         -------
-        an array of length of the E array with each element being the 
-        D matrix after computing Earth-matter effects
+        D : an array of length of the E array of 6 x 6 matrices
         """
         U = self.mix_params.VacuumMixingMatrix()
 
@@ -1082,7 +1079,7 @@ class FourFlavorNoEarthMatter(FourFlavorTransformation):
 
         Returns
         -------
-        an array of 8 x 8 matrices of length equal to the length of the E array
+        D : an array of 8 x 8 matrices of length equal to the length of the E array
         """
         U = self.mix_params.VacuumMixingMatrix()
 
@@ -1116,6 +1113,7 @@ class EarthMatter(ThreeFlavorTransformation):
         self.SNAltAz = SNAltAz
 
         self.prior_E = None # used to store energy array from previous calls to get_probabilities
+        self.prior_D = None
 
     def __str__(self):
         return f'EarthMatter_' + str(self.mix_params.mass_order)
@@ -1132,18 +1130,17 @@ class EarthMatter(ThreeFlavorTransformation):
 
         Returns
         -------
-        an array of length of the E array with each element being the 
-        D matrix after computing Earth-matter effects
+        D : an array of length of the E array with each element being a 6 x 6 matrix
         """
         if EMEWS == None:
             print("The EMEWS module cannot be found. Results do not include the Earth-matter effect.")
-            return self.D
+            return ThreeFlavorNoEarthMatter(self.mix_params).get_probabilities(t,E)
 
         if self.prior_E != None:
             if u.allclose(self.prior_E, E) == True:
-                return self.D
+                return self.prior_D
 
-        self.D = np.zeros((6,6,len(E))) 
+        self.prior_D = np.zeros((6,6,len(E))) 
         self.prior_E = E
         
         #input data object for EMEWS
@@ -1176,31 +1173,30 @@ class EarthMatter(ThreeFlavorTransformation):
         #matrix from EMEWS needs to be rearranged to match SNEWPY flavor indicii ordering
         Pfm = EMEWS.Run(ID)
 
-        m = 0
-        while m<len(E):
-            self.D[ThreeFlavor.NU_E,0,m] = Pfm[0][m][0][0] 
-            self.D[ThreeFlavor.NU_E,1,m] = Pfm[0][m][0][1]
-            self.D[ThreeFlavor.NU_E,2,m] = Pfm[0][m][0][2]
-            self.D[ThreeFlavor.NU_MU,0,m] = Pfm[0][m][1][0] 
-            self.D[ThreeFlavor.NU_MU,1,m] = Pfm[0][m][1][1]
-            self.D[ThreeFlavor.NU_MU,2,m] = Pfm[0][m][1][2]
-            self.D[ThreeFlavor.NU_TAU,0,m] = Pfm[0][m][2][0] 
-            self.D[ThreeFlavor.NU_TAU,1,m] = Pfm[0][m][2][1]
-            self.D[ThreeFlavor.NU_TAU,2,m] = Pfm[0][m][2][2]
+        for m in range(len(E)):
+            self.prior_D[ThreeFlavor.NU_E,0,m] = Pfm[0][m][0][0] 
+            self.prior_D[ThreeFlavor.NU_E,1,m] = Pfm[0][m][0][1]
+            self.prior_D[ThreeFlavor.NU_E,2,m] = Pfm[0][m][0][2]
+            self.prior_D[ThreeFlavor.NU_MU,0,m] = Pfm[0][m][1][0] 
+            self.prior_D[ThreeFlavor.NU_MU,1,m] = Pfm[0][m][1][1]
+            self.prior_D[ThreeFlavor.NU_MU,2,m] = Pfm[0][m][1][2]
+            self.prior_D[ThreeFlavor.NU_TAU,0,m] = Pfm[0][m][2][0] 
+            self.prior_D[ThreeFlavor.NU_TAU,1,m] = Pfm[0][m][2][1]
+            self.prior_D[ThreeFlavor.NU_TAU,2,m] = Pfm[0][m][2][2]
 
-            self.D[ThreeFlavor.NU_E_BAR,3,m] = Pfm[1][m][0][0] 
-            self.D[ThreeFlavor.NU_E_BAR,4,m] = Pfm[1][m][0][1]
-            self.D[ThreeFlavor.NU_E_BAR,5,m] = Pfm[1][m][0][2]
-            self.D[ThreeFlavor.NU_MU_BAR,3,m] = Pfm[1][m][1][0] 
-            self.D[ThreeFlavor.NU_MU_BAR,4,m] = Pfm[1][m][1][1]
-            self.D[ThreeFlavor.NU_MU_BAR,5,m] = Pfm[1][m][1][2]
-            self.D[ThreeFlavor.NU_TAU_BAR,3,m] = Pfm[1][m][2][0] 
-            self.D[ThreeFlavor.NU_TAU_BAR,4,m] = Pfm[1][m][2][1]
-            self.D[ThreeFlavor.NU_TAU_BAR,5,m] = Pfm[1][m][2][2]
-            m += 1
+            self.prior_D[ThreeFlavor.NU_E_BAR,3,m] = Pfm[1][m][0][0] 
+            self.prior_D[ThreeFlavor.NU_E_BAR,4,m] = Pfm[1][m][0][1]
+            self.prior_D[ThreeFlavor.NU_E_BAR,5,m] = Pfm[1][m][0][2]
+            self.prior_D[ThreeFlavor.NU_MU_BAR,3,m] = Pfm[1][m][1][0] 
+            self.prior_D[ThreeFlavor.NU_MU_BAR,4,m] = Pfm[1][m][1][1]
+            self.prior_D[ThreeFlavor.NU_MU_BAR,5,m] = Pfm[1][m][1][2]
+            self.prior_D[ThreeFlavor.NU_TAU_BAR,3,m] = Pfm[1][m][2][0] 
+            self.prior_D[ThreeFlavor.NU_TAU_BAR,4,m] = Pfm[1][m][2][1]
+            self.prior_D[ThreeFlavor.NU_TAU_BAR,5,m] = Pfm[1][m][2][2]
 
-        return self.D
+        return self.prior_D
 
+###############################################################################
 
 class Catenate:
     """Catenate flavor transformation effects together."""
@@ -1209,11 +1205,10 @@ class Catenate:
         """        
         Parameters
         ----------
-        SNTransformation : the transformation that occurs in the supernova
-        InVacuumTransformation : the transforamtion that occurs in the vacuum
-        AtEarthTransformation : the transformation that occurs at Earth
-        The order is that SNTransformation is applied first, then InVacuumTransformation, 
-        then AtEarthTransformation
+        SNTransformation : a FlavorTransformation object that describes the transformation that occurs in the supernova
+        InVacuumTransformation : a FlavorTransformation object that describes the transforamtion that occurs in the vacuum
+        AtEarthTransformation : a FlavorTransformation object that describes the transformation that occurs at Earth
+        The order is that SNTransformation is applied first, then InVacuumTransformation, then AtEarthTransformation
         """  
         self.transform1 = SNTransformation
         
