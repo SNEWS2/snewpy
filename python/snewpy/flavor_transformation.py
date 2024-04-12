34d887ed9e0ffd3de82cdd806763e3a727e93dd6
jpkneller <jpknelle@ncsu.edu>
2024-04-12 09:48:37 -0400

    Update flavor_transformation.py

--- python/snewpy/flavor_transformation.py
+++ python/snewpy/flavor_transformation.py
@@ -1004,10 +1004,10 @@ class QuantumDecoherence(ThreeFlavorTransformation):
         """        
         PQD = np.zeros((6,6,len(E)))
 
-        PQD[0,0] = 1/3 + 1/2 * np.exp(-(self.Gamma3 * (E/self.E0)**self.n + self.Gamma8 * (E/self.E0)**self.n / 3) * self.d) \
+        PQD[0,0] = 1/3 + 1/2 * np.exp(-self.Gamma3 * (E/self.E0)**self.n * self.d) \
                   + 1/6 * np.exp(-self.Gamma8 * (E/self.E0)**self.n * self.d)
 
-        PQD[0,1] = 1/3 - 1/2 * np.exp(-(self.Gamma3 * (E/self.E0)**self.n + self.Gamma8 * (E/self.E0)**self.n / 3) * self.d) \
+        PQD[0,1] = 1/3 - 1/2 * np.exp(-self.Gamma3 * (E/self.E0)**self.n * self.d) \
                   + 1/6 * np.exp(-self.Gamma8 * (E/self.E0)**self.n * self.d)
 
         PQD[0,2] = 1/3 - 1/3 * np.exp(-self.Gamma8 * (E/self.E0)**self.n * self.d)
