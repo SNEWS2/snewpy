0efaf6f3e0d78eee6d84d88afdeb1f358bbc20bb
Andrey Sheshukov <dxdydz2@gmail.com>
2024-05-10 10:36:15 +0300

    Update tests

--- python/snewpy/test/test_flavors.py
+++ python/snewpy/test/test_flavors.py
@@ -1,7 +1,9 @@
 import pytest
 import numpy as np
-from snewpy import flavor
-from snewpy.flavor import TwoFlavor,ThreeFlavor,FourFlavor
+import snewpy.flavor
+from snewpy.flavor import TwoFlavor,ThreeFlavor,FourFlavor, FlavorMatrix, FlavorScheme
+
+flavor_schemes = TwoFlavor,ThreeFlavor,FourFlavor
 
 class TestFlavorScheme:
     @staticmethod
@@ -11,6 +13,7 @@ class TestFlavorScheme:
         assert len(FourFlavor)==8
         
     @staticmethod
+    
     def test_getitem_string():
         assert TwoFlavor['NU_E'] == TwoFlavor.NU_E
         assert TwoFlavor['NU_X'] == TwoFlavor.NU_X
@@ -31,7 +34,7 @@ class TestFlavorScheme:
     
     @staticmethod
     def test_makeFlavorScheme():
-        TestFlavor = flavor.makeFlavorScheme('TestFlavor',['A','B','C'])
+        TestFlavor = FlavorScheme.from_lepton_names('TestFlavor',leptons=['A','B','C'])
         assert len(TestFlavor)==6
         assert [f.name for f in TestFlavor]==['NU_A','NU_A_BAR','NU_B','NU_B_BAR','NU_C','NU_C_BAR']
 
@@ -82,16 +85,31 @@ class TestFlavorScheme:
 class TestFlavorMatrix:
     @staticmethod
     def test_init_square_matrix():
-        m = flavor.FlavorMatrix(array=np.ones(shape=(4,4)), flavors=TwoFlavor)
+        m = FlavorMatrix(array=np.ones(shape=(4,4)), flavor=TwoFlavor)
         assert m.shape == (4,4)
-        assert m.flavors_from == TwoFlavor
-        assert m.flavors_to == TwoFlavor
+        assert m.flavor_in == TwoFlavor
+        assert m.flavor_out == TwoFlavor
         
     @staticmethod
     def test_init_square_matrix_with_wrong_shape_raises_ValueError():
         with pytest.raises(ValueError):
-            m = flavor.FlavorMatrix(array=np.ones(shape=(4,5)), flavors=TwoFlavor)
+            m = FlavorMatrix(array=np.ones(shape=(4,5)), flavor=TwoFlavor)
         with pytest.raises(ValueError):
-            m = flavor.FlavorMatrix(array=np.ones(shape=(5,5)), flavors=TwoFlavor)
+            m = FlavorMatrix(array=np.ones(shape=(5,5)), flavor=TwoFlavor)
         with pytest.raises(ValueError):
-            m = flavor.FlavorMatrix(array=np.ones(shape=(5,4)), flavors=TwoFlavor)
+            m = FlavorMatrix(array=np.ones(shape=(5,4)), flavor=TwoFlavor)
+
+    @staticmethod
+    def test_conversion_matrices_for_same_flavor_are_unity():
+        for flavor in [TwoFlavor,ThreeFlavor,FourFlavor]:
+            matrix = FlavorMatrix.conversion_matrix(flavor,flavor)
+            print(matrix)
+            assert np.allclose(matrix.array, np.eye(len(flavor)))
+
+    @staticmethod
+    @pytest.mark.parametrize('flavor_in',flavor_schemes)
+    @pytest.mark.parametrize('flavor_out',flavor_schemes)
+    def test_conversion_matrices(flavor_in, flavor_out):
+        M = FlavorMatrix.conversion_matrix(flavor_out,flavor_in)
+        assert M.flavor_in == flavor_in
+        assert M.flavor_out == flavor_out
