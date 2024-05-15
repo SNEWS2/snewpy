9c995c3fb59fe4a72d2adae70d542649d1078cb7
Jost Migenda <jost.migenda@kcl.ac.uk>
2024-05-15 19:27:39 +0200

    mark tests that use `get_transformed_flux` as xfail

--- python/snewpy/test/test_rate_calculation.py
+++ python/snewpy/test/test_rate_calculation.py
@@ -42,6 +42,7 @@ def fluence(snmodel):
     flux = snmodel.get_flux(t=times, E=energies, distance=10<<u.kpc, flavor_xform=ft.AdiabaticMSW())
     return flux.integrate('time')
 
+@pytest.mark.xfail(reason="Fluence calculation uses `get_transformed_flux`, which is currently hard coded to a TwoFlavor scheme.", raises=AttributeError)
 @pytest.mark.parametrize('detector, expected_total',crosscheck_table_unsmeared)
 def test_rate_unsmeared(rc, fluence, detector, expected_total):    
     #calculate the event rates
@@ -52,6 +53,7 @@ def test_rate_unsmeared(rc, fluence, detector, expected_total):
     #check the final value
     assert N_total == pytest.approx(expected_total, 0.01)
     
+@pytest.mark.xfail(reason="Fluence calculation uses `get_transformed_flux`, which is currently hard coded to a TwoFlavor scheme.", raises=AttributeError)
 @pytest.mark.parametrize('detector, expected_total',crosscheck_table)
 def test_rate_smeared(rc, fluence, detector, expected_total):
     #calculate the event rates
