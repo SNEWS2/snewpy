7ccbc3843ba1c189c74c08c2bafbc261e6f7d671
Andrey Sheshukov <dxdydz2@gmail.com>
2024-08-01 11:47:16 +0300

    Update _repr_markdown_ to prevent creating unnecessary headers

--- python/snewpy/flavor.py
+++ python/snewpy/flavor.py
@@ -159,13 +159,11 @@ class FlavorMatrix:
         res = [f'**{self.__class__.__name__}**:<`{self.flavor_in.__name__}`->`{self.flavor_out.__name__}`> shape={self.shape}','']
         flavors0 = [f.to_tex() for f in self.flavor_in]
         flavors1 = [f.to_tex() for f in self.flavor_out]
-        hdr = '|'.join(['*']+flavors1)
-        res+=[hdr]
+        res+=['|'.join(['*']+flavors1)]
         res+=['|'.join(['-:',]*(len(flavors1)+1))]
         for f0 in self.flavor_in:
             line = [f0.to_tex()]+[f'{v:.3g}' for v in self[:,f0]]
             res+=['|'.join(line)]
-        res+=['---------']
         return '\n'.join(res)
     
     def __eq__(self,other):
