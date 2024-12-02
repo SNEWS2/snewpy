99c45348ad49b309a6e86914b52ce92b8961c035
Andrey Sheshukov <dxdydz2@gmail.com>
2024-12-02 17:19:15 +0300

    Adding version checks

--- python/snewpy/test/test_00_init.py
+++ python/snewpy/test/test_00_init.py
@@ -1,7 +1,14 @@
 import snewpy
-import unittest
+import importlib
+import subprocess
 
+def test_version_exists():
+    assert hasattr(snewpy, '__version__')
 
-class TestInit(unittest.TestCase):
-    def test_version_exists(self):
-        self.assertTrue(hasattr(snewpy, '__version__'))
+def test_version_is_consistent_with_variable():
+    assert importlib.metadata.version('snewpy') == snewpy.__version__
+
+def test_version_is_consistent_with_git_tag():
+    git_tag = subprocess.check_output('git describe --abbrev=0'.split())
+    git_tag = git_tag.decode('ascii').strip().lstrip('v')
+    assert git_tag == snewpy.__version__
