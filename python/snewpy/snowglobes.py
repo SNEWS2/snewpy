8d3fe8085bfdd33b8ee4ed8d5c8661fff8111bcd
jpkneller <jpknelle@ncsu.edu>
2025-09-02 11:03:39 -0400

    Update snowglobes.py

--- python/snewpy/snowglobes.py
+++ python/snewpy/snowglobes.py
@@ -127,7 +127,6 @@ def generate_time_series(model_path, model_type, flavor_transformation, d, outpu
         Path of NumPy archive file with neutrino fluence data.
     """
     all_models_dict = get_all_models_dict()
-    print(all_models_dict)
     model_class = all_models_dict[model_type]
 
     # if flavor_transformation is a string, find the appropriate class
