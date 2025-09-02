7f95cb6c986a5dab9f10c5d2258e05e4a434b82b
jpkneller <jpknelle@ncsu.edu>
2025-09-01 20:11:10 -0400

    Update snowglobes.py

--- python/snewpy/snowglobes.py
+++ python/snewpy/snowglobes.py
@@ -170,6 +170,7 @@ def generate_fluence(model_path, model_type, flavor_transformation, d, output_fi
         Path of NumPy archive file with neutrino fluence data.
     """
     all_models_dict = get_all_models_dict()
+    print(all_models_dict)
     model_class = getattr(all_models_dict, model_type)
 
     # if flavor_transformation is a string, find the appropriate class
