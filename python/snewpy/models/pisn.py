692fd820dd8fb6c343067955a2a6edee431a116b
jpkneller <jpknelle@ncsu.edu>
2025-11-05 08:38:31 -0500

    Update pisn.py
    new Wright_2017 model class for PISN models

--- python/snewpy/models/pisn.py
+++ python/snewpy/models/pisn.py
@@ -2,15 +2,22 @@
 """
 The submodule ``snewpy.models.pisn`` contains models of pair-instability supernova neutrino fluxes,
 """
-from snewpy.models.base import SupernovaModel
-from snewpy.models.registry_model import RegistryModel
+import logging
+import os
+
 from astropy import units as u
+
+from snewpy.models import pisn_loaders as loaders
+from .base import SupernovaModel
+
+from snewpy.models.registry_model import RegistryModel, Parameter
+from snewpy.models.registry_model import all_models
 
 @RegistryModel(
     progenitor_mass = [150, 250] * u.Msun,
-    eos = ['SFHo', 'Helm'],    
+    eos = ['SFHo', 'Helm']    
 )
-class Wright_2017(SupernovaModel):
+class Wright_2017(loaders.Wright_2017):
     """PISN model described in the paper `Neutrino signal from pair-instability supernovae' by Wright et al.,  
     Phys. Rev. D96 (2017) 103008  <https://journals.aps.org/prd/abstract/10.1103/PhysRevD.96.103008>`_
     """
@@ -24,3 +31,5 @@ class Wright_2017(SupernovaModel):
     def __init__(self, progenitor_mass:u.Quantity, eos:str):
         filename=f"PISN_{progenitor_mass.to_value('Msun'):.0f}Msun_EOS={eos}_NeutrinoFlux.tar.bz2"
         super().__init__(filename, self.metadata)
+
+    
