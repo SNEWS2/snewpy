f22d33200bdc5e9248367419dd103211d21620d2
jpkneller <jpknelle@ncsu.edu>
2026-08-10 09:21:37 -0400

    Update ccsn.py

--- python/snewpy/models/ccsn.py
+++ python/snewpy/models/ccsn.py
@@ -457,26 +457,3 @@ class Bugli_2021(loaders.Bugli_2021):
             filename = f'{Bfield}_b12_3d_{rotation}deg_snewpy_{direction}.dat'
         return super().__init__(filename=filename, metadata=self.metadata)
 
-
-class Analytic3Species(PinchedModel):
-    """An analytical model calculating spectra given total luminosity,
-    average energy, and rms or pinch, for each species.
-    """
-
-    param = "There are no input files available for this class. Use `doc/scripts/Analytic.py` in the SNEWPY GitHub repo to create a custom input file."
-
-    def get_param_combinations(cls):
-        print(cls.param)
-        return []
-
-    def __init__(self, filename):
-        """
-        Parameters
-        ----------
-        filename : str
-            Absolute or relative path to file with model data.
-        """
-
-        simtab = Table.read(filename,format='ascii')
-        self.filename = filename
-        super().__init__(simtab, metadata={})
