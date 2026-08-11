844220630c0bcbe2170acddae264296af8caa6e0
jpkneller <jpknelle@ncsu.edu>
2026-08-11 14:10:30 -0400

    Update ccsn_loaders.py

--- python/snewpy/models/ccsn_loaders.py
+++ python/snewpy/models/ccsn_loaders.py
@@ -1117,3 +1117,25 @@ class Fischer_2020(PinchedModel):
         super().__init__(simtab, metadata)
 
 
+class PinchedModel(base.PinchedModel):
+    """An analytical model calculating spectra given total luminosity,
+    average energy, and rms or pinch, for each species.
+    """
+
+    param = "There are no input files available for this class. Use `doc/scripts/Analytic.py` in the SNEWPY GitHub repo to create a custom input file."
+
+    def get_param_combinations(cls):
+        print(cls.param)
+        return []
+
+    def __init__(self, filename):
+        """
+        Parameters
+        ----------
+        filename : str
+            Absolute or relative path to file with model data.
+        """
+
+        simtab = Table.read(filename,format='ascii')
+        self.filename = filename
+        super().__init__(simtab, metadata={})
