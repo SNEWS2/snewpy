d0337374c513d9a57a7d9bb2da82a749951b12bd
jpkneller <jpknelle@ncsu.edu>
2026-08-11 15:23:47 -0400

    Update ccsn.py

--- python/snewpy/models/ccsn.py
+++ python/snewpy/models/ccsn.py
@@ -50,7 +50,7 @@ class Ebinger_2018(loaders.PUSHArchiveModel):
     """Model from the PUSH collaboration described in Ebinger et al.
     """
     def __init__(self, progenitor_mass:u.Quantity, eos:str='SFHo', callibration:str)
-        filename = f's{progenitor_mass.value:3.1f}._{eos}_{calibration}_Ebinger_luminosity.h5'
+        filename = f's{progenitor_mass.value:3.1f}._{eos}_{callibration}_Ebinger_luminosity.h5'
         return super().__init__(filename, self.metadata)
 
 @RegistryModel(
@@ -62,7 +62,7 @@ class Curtis_2019(loaders.PUSHArchiveModel):
     """Model from the PUSH collaboration described in Curtis et al.
     """
     def __init__(self, progenitor_mass:u.Quantity, eos:str='SFHo', callibration:str):
-        filename = f's{progenitor_mass.value:3.1f}._{eos}_{calibration}_Curtis_luminosity.h5'
+        filename = f's{progenitor_mass.value:3.1f}._{eos}_{callibration}_Curtis_luminosity.h5'
         return super().__init__(filename, self.metadata)
 
      
