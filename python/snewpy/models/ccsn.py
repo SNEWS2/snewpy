57027cf84b72fcb50557a63d72eb9c81a1fd4359
jpkneller <jpknelle@ncsu.edu>
2026-08-11 14:26:40 -0400

    Update ccsn.py

--- python/snewpy/models/ccsn.py
+++ python/snewpy/models/ccsn.py
@@ -44,25 +44,25 @@ from textwrap import dedent
 @RegistryModel(
     progenitor_mass= [11.2, 27.] * u.Msun,
     eos = ['SFHo'],
-    callibration = ['cal1', 'cal2'] 
+    callibration = ['calI', 'calII'] 
 )
 class Ebinger_2018(loaders.PUSHArchiveModel):
     """Model from the PUSH collaboration described in Ebinger et al.
     """
     def __init__(self, progenitor_mass:u.Quantity), eos:str='SFHo', callibration:str)
-        filename = f's{progenitor_mass.value:3.1f}._{eos}_{calibration}_Ebinger_luminosity.h'
+        filename = f's{progenitor_mass.value:3.1f}._{eos}_{calibration}_Ebinger_luminosity.h5'
         return super().__init__(filename, self.metadata)
 
 @RegistryModel(
     progenitor_mass= [11.2, 27.] * u.Msun,
     eos = ['SFHo'],
-    callibration = ['cal1'] 
+    callibration = ['calI', 'calII'] 
 )
 class Curtis_2019(loaders.PUSHArchiveModel):
     """Model from the PUSH collaboration described in Curtis et al.
     """
     def __init__(self, progenitor_mass:u.Quantity, eos:str='SFHo', callibration:str):
-        filename = f's{progenitor_mass.value:3.1f}._{eos}_{calibration}_Curtis_luminosity.h'
+        filename = f's{progenitor_mass.value:3.1f}._{eos}_{calibration}_Curtis_luminosity.h5'
         return super().__init__(filename, self.metadata)
 
      
