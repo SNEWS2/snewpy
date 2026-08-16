4d4e9bea5720ccaa43135ee7e3f43f2eb0e1ee44
jpkneller <jpknelle@ncsu.edu>
2026-08-15 20:40:07 -0400

    Update TimeSeries.py

--- doc/scripts/TimeSeries.py
+++ doc/scripts/TimeSeries.py
@@ -23,5 +23,5 @@ detector = "wc100kt30prct"
 rc = RateCalculator()
 events = rc.run(fluence, detector, detector_effects=True)
 
-filename = f"{model}.{transformation}.{times[0]:.3f}-{times[-1]:.3f},{energies[0]:.3f}-{energies[-1]:.3f},{distance:.3f}.npz"
-np.savez(filename,events)
+filename = f"{model}.{transformation}.{times[0]:.3f}-{times[-1]:.3f},{energies[0]:.3f}-{energies[-1]:.3f},{distance:.3f}.{detector}.npz"
+np.savez(filename,**events)
