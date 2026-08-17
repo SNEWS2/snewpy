f44ff6509858441b8284b9fcc3df0f73e68b8d15
jpkneller <jpknelle@ncsu.edu>
2026-08-16 20:39:34 -0400

    Update flux.py

--- python/snewpy/flux.py
+++ python/snewpy/flux.py
@@ -149,7 +149,8 @@ class _ContainerBase:
             
         Nf,Nt,Ne = len(self.flavor), len(self.time), len(self.energy)
         #list all valid shapes of the input array
-        expected_shapes=[(nf,nt,ne) for nf in (Nf,Nf-1) for nt in (Nt,Nt-1) for ne in (Ne,Ne-1)]
+        expected_shapes=[(Nf,nt,ne) for nt in (Nt,Nt-1) for ne in (Ne,Ne-1)]
+        
         #treat special case if data is 1d array
         if self.array.ndim==1:
             #try to reshape the array to expected shape
