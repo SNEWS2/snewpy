c867971fdf4a07e2a9dd4fb04f0e1b37dcf07e05
Jost Migenda <jost.migenda@kcl.ac.uk>
2026-08-20 19:17:13 +0100

    update docstrings for *MixingParameters classes/function

--- python/snewpy/neutrino.py
+++ python/snewpy/neutrino.py
@@ -36,10 +36,15 @@ class MassHierarchy(IntEnum):
 @dataclass
 class ThreeFlavorMixingParameters(Mapping):
     """Mixing angles and mass differences, assuming three neutrino flavors.
-    This class contains the default values used throughout SNEWPY, which are
-    based on `NuFIT 6.0 <http://www.nu-fit.org>`_ results from September 2024,
-    published in `JHEP 12 (2024) 216 <https://dx.doi.org/10.1007/JHEP12(2024)216>`_
-    [`arXiv:2410.05380 <https://arxiv.org/abs/2410.05380>`_].
+
+    In most cases, you should use the :func:`MixingParameters` function to get
+    a preset set of parameters, rather than constructing this class directly:
+    
+        >>> pars = MixingParameters(mass_order='NORMAL')
+    
+    If desired, you can then update the parameters using the :meth:`update` method:
+    
+        >>> pars.update(theta12=34<<u.deg, dm21_2=7.5e-5<<u.eV**2)
     """
     #mixing angles
     theta12: u.Quantity[u.deg]
@@ -223,7 +228,7 @@ class FourFlavorMixingParameters(ThreeFlavorMixingParameters):
     """A class for four flavor neutrino mixing. 
     ..Note: it is an extension of :class:`ThreeFlavorMixingParameters`, and can be constructed using it:
     
-        >>> pars_3f = ThreeFlavorMixingParameters() #standard 3flavor mixing
+        >>> pars_3f = ThreeFlavorMixingParameters() #standard 3flavor mixing # TODO: this fails with a TypeError!
         >>> pars_4f = FourFlavorMixingParameters(**pars_3f, theta14=90<<u.deg, dm41_2=1<<u.GeV**2)
     """
     #sterile neutrino mixing angles. 
@@ -526,6 +531,18 @@ parameter_presets = {
    
 
 def MixingParameters(mass_order:MassHierarchy|str='NORMAL', version:str='NuFIT6.0'):
+    """Neutrino mixing parameters presets based on results from
+    `NuFIT <http://www.nu-fit.org>`_ or `PDG <https://pdglive.lbl.gov>`_.
+
+    Parameters:
+    -----------
+    mass_order : MassHierarchy or str, optional
+        Neutrino mass ordering, either ``MassHierarchy.NORMAL`` (default) or ``MassHierarchy.INVERTED``.
+    version : str, optional
+        Version of the preset values. Default is ``NuFIT6.0``.
+        Other available versions are ``NuFIT5.0``, ``NuFIT5.2``, ``NuFIT6.1``,
+        ``PDG2022``, ``PDG2024``, and ``PDG2026``.
+    """
     if isinstance(mass_order,str):
         mass_order = MassHierarchy[mass_order]
     return parameter_presets[version][mass_order]
