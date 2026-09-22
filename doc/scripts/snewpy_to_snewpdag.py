12df6c70b22e3a06b263303ebf6262517c22c2db
jpkneller <jpknelle@ncsu.edu>
2026-09-21 20:04:18 -0400

    Update snewpy_to_snewpdag.py

--- doc/scripts/snewpy_to_snewpdag.py
+++ doc/scripts/snewpy_to_snewpdag.py
@@ -1,59 +1,39 @@
 
-from snewpy import snowglobes
+from snewpy.models.ccsn import Tamborra_2014
+from snewpy.neutrino import MassHierarchy, MixingParameters
+from snewpy.flavor_transformation import AdiabaticMSW
+from snewpy.rate_calculator import RateCalculator, center
+
 import numpy as np
-from astropy import units as u
+import astropy.units as u
 
-SNOwGLoBES_path = None  # change to SNOwGLoBES directory if using a custom detector configuration
-SNEWPY_models_base = "/location/of/models/" #where models (aka input for to_snowglobes) is located
-output_path = "/path/to/output/" #where the output files will be located
+model = Tamborra_2014(progenitor_mass=20*u.solMass, direction=1)
+transformation = AdiabaticMSW(MixingParameters('NORMAL')) # Desired flavor transformation
+       
+times    = model.get_time()
+energies = np.linspace(0,100,501)<<u.MeV
+distance = 10*u.kpc
 
-#set distance in kpc
-distance=10
-
-#set SNOwGLoBES detector to use
-detector = "icecube"
-
-#set SNEWPY model type and filename
-modeltype = 'Tamborra_2014'
-modeldir = SNEWPY_models_base+"/"+modeltype+"/"
-model = 's20.0c_3D_dir1'
-filename = model
-
-#set desired flavor transformation prescription
-transformation = 'AdiabaticMSW_NMO'
-
-#snewpy.snowglobes creates a tarred file of snowglobes fluences
-#this is stored in the model types directory in snewpy/models
-outfile = modeltype+"_"+model+"_"+transformation
-
-#Specify sequence of time intervals, one fluence
-#file is made for each elements with dt=tstart[i]-tend[i]
+#Specify sequence of time intervals
 window_tstart = 0.001
 window_tend = 0.331
 window_bins = 330
-tstart = np.linspace(window_tstart,window_tend,window_bins,endpoint=False)*u.s
-tend = tstart + (window_tend-window_tstart)/window_bins*u.s
-tmid = (tstart+tend)*0.5
+tbins =  np.linspace(window_tstart,window_tend,window_bins) * u.s
+                     
+#get the flux from the model
+flux = model.get_flux(t=times, E=energies, distance=distance, flavor_xform=transformation)
+fluence = flux.integrate('time', limits=tbins)
 
-#Generate fluence file for SNOwGLoBES (there are two options, here we use generate_fluence)
-print("Preparing fluences...")
-tarredfile = snowglobes.generate_fluence(modeldir+filename, modeltype, transformation, distance, outfile,tstart,tend)
-print("Done fluences...")
+#set SNOwGLoBES detector to use
+detector = "icecube"
+rc = RateCalculator()
+tables = rc.run(fluence, detector, detector_effects=True)
 
-print("Running snowglobes...")
-#now run SNOwGLoBES, this will loop over all the fluence files in `tarredfile`
-snowglobes.simulate(SNOwGLoBES_path, tarredfile, detector_input=detector)
-print("Done snowglobes...")
+output_path = "/path/to/output/" #where the output files will be located
+fout = open(output_path+"snewpy_output_"+detector+"_Tamborra2014_20solMass,direction=1_1msbin.txt", "a")
 
-#now collate results of output of SNOwGLoBES
-print("Collating...")
-tables = snowglobes.collate(tarredfile, skip_plots=True)
-
-#read results from SNOwGLoBES and put lightcurve in output file for snewpdag
-fout = open(output_path+"snewpy_output_"+detector+"_"+modeltype+"_"+filename+"_1msbin.txt", "a")
+tmid = center(tbins)
 nevents = np.zeros(len(tmid))
 for i in range(len(tmid)):
-    key = "Collated_"+outfile+"_"+str(i)+"_"+detector+"_events_smeared_weighted.dat"
-    for j in range(1,len(tables[key]['header'].split())):
-        nevents[i] += sum(tables[key]['data'][j])
+    nevents[i] = sum([(chan.integrate_or_sum('energy').array.squeeze())[i].value for chan in tables.values()])
     print(i, "\t" , nevents[i], file=fout)
