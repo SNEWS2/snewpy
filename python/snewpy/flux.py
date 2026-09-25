6a48ef676331fbd2c46e86e921620804a9418ce8
jpkneller <jpknelle@ncsu.edu>
2026-09-25 13:57:37 -0400

    Update flux.py

--- python/snewpy/flux.py
+++ python/snewpy/flux.py
@@ -281,7 +281,7 @@ class _ContainerBase:
         cumsum_limits = (cumsum.take(0,axis=axis), cumsum.take(-1,axis=axis))  
 
         # interpolate the cumulative sum on the right bin edges
-        _interpolator = interp1d(x=ax[1:], y=cumsum, fill_value=cumsum_limits, axis=axis, bounds_error=False)
+        _interpolator = interp1d(x=ax, y=cumsum, fill_value=cumsum_limits, axis=axis, bounds_error=False)
 
         # Evaluate interpolator at new bin limits, then difference to give the counts in the new bins
         new_array = np.diff(_interpolator(limits),axis=axis) << self.array.unit      
